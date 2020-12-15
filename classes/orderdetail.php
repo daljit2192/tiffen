8d24cbecea5d6f8a84e6deccf2bd1e0412041e84
Pooja Sharma <pooja.sharma@evry.com>
2020-12-15 10:56:22 +0530

    Update orderdetail.php

--- classes/orderdetail.php
+++ classes/orderdetail.php
@@ -5,14 +5,27 @@ class OrderDetail extends Connect{
     function generateOrder($plan_array){
 		for($i=0;$i<count($plan_array);$i++) {
 			$planid=$plan_array[$i]['planid'];
-			for($j=0;$j<$plan_array[$i]['count'];$j++){
-				$sql = "INSERT INTO order_details (assigned_plan_id) 
-				VALUES ('".$planid."');";
-				$this->conn->query($sql); 
-			}
+			$count=(int)$plan_array[$i]['count'];
+			$sql = "INSERT INTO order_details (assigned_plan_id,days,status_id) 
+			VALUES ('".$planid."',$count,1);";
+			$this->conn->query($sql); 
+			
 		}
 		return $this->order_detail;
-    }
+	}
+	function getOrderDetails(){
+		$sql = "SELECT * FROM order_details as od inner join assigned_plans as ap on od.assigned_plan_id=ap.id";
+        $result = $this->conn->query($sql); 
+        
+		if ($result->num_rows > 0) {
+	        while($row = $result->fetch_assoc()) {
+    			array_push($this->order_detail['data'],$row);
+  			}
+        } else {
+        	$this->order_detail['error'] = "No meals found";
+        }
+		return $this->order_detail;
+	}
     
 }
 ?>
